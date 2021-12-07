c6b4e37fed1ed49c425891b4ed73974de9f734c5
Evgeniy Talko <etalko@gmail.com>
2021-12-07 19:13:17 +0300

    add(toggle-nav): interaction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,51 @@ devLogEntryLoaded();
 
 const siteHead = document.querySelector('.site-head');
 const navToggle = document.querySelector('.nav-toggle');
+const navMain = document.querySelector('.nav--main');
+const socialLinksWrapper = document.querySelector('.social-links--wrapper');
 const burgerOpened = 'burger-opened';
 
-function toggleMainNav (navToggle) {
+function initMainNavToggler (navToggle) {
     navToggle.addEventListener('click', (_) => {
-        siteHead.classList.toggle(burgerOpened);
+        let menuOpened = siteHead.classList.contains(burgerOpened);
+        socialLinksWrapper.style.opacity = menuOpened ? '0' : '100';
+
+        setTimeout(() => {
+            socialLinksWrapper.style.display = menuOpened ? 'none' : 'block';
+        }, 1000)
+
+
+        if (menuOpened) {
+            siteHead.classList.remove(burgerOpened)
+            navMain.classList.add('sr-only');
+            navMain.classList.remove('not-sr-only');
+            // socialLinksWrapper.classList.add('opacity-0');
+            // socialLinksWrapper.classList.remove('opacity-100');
+
+            setTimeout(() => {
+                // debugger;
+                // navMain.classList.add('opacity-0');
+                // navMain.classList.remove('opacity-100');
+                // socialLinksWrapper.classList.add('hidden');
+                // socialLinksWrapper.classList.remove('block');
+            }, 500)
+
+            return;
+        }
+
+        siteHead.classList.add(burgerOpened)
+        // navMain.classList.add('opacity-100');
+        // navMain.classList.remove('opacity-0');
+        // socialLinksWrapper.classList.add('block');
+        // socialLinksWrapper.classList.remove('hidden');
+        // socialLinksWrapper.classList.add('opacity-100');
+        // socialLinksWrapper.classList.remove('opacity-0');
+
+        setTimeout(() => {
+            navMain.classList.add('not-sr-only');
+            navMain.classList.remove('sr-only');
+        }, 500)
+
     })
 }
 
@@ -18,7 +58,7 @@ function main() {
     fixViewportHeight();
 
     // >>> Place your code there
-    toggleMainNav(navToggle);
+    initMainNavToggler(navToggle);
     // <<< Place your code there
 }
 
