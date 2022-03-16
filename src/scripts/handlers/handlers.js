77cd52c2ee9cca062fd73116d34f829b981562b7
Evgeniy Talko <etalko@gmail.com>
2022-03-16 17:20:30 +0300

    style(handlers): format code

--- src/scripts/handlers/handlers.js
+++ src/scripts/handlers/handlers.js
@@ -1,54 +1,53 @@
 // import { AsYouType } from "libphonenumber-js";
-import { printFormData } from "/@input/scripts/utils";
+import { printFormData } from '/@input/scripts/utils';
 
 function showPopupContainer(containerSelector, pageContentSelector) {
-  const $popupContainer = document.querySelector(containerSelector);
-  document.body.style.overflowY = "hidden";
-  document.querySelector(pageContentSelector).classList.add("_blur");
-  document.querySelector("footer").classList.add("_blur");
-  $popupContainer.style.display = "block";
+    const $popupContainer = document.querySelector(containerSelector);
+    document.body.style.overflowY = 'hidden';
+    document.querySelector(pageContentSelector).classList.add('_blur');
+    document.querySelector('footer').classList.add('_blur');
+    $popupContainer.style.display = 'block';
 }
 
 function hidePopupContainer(containerSelector, pageContentSelector) {
-  const $popupContainer = document.querySelector(containerSelector);
-  document.body.style.overflowY = "auto";
-  $popupContainer.style.display = "none";
-  document.querySelector(pageContentSelector).classList.remove("_blur");
-  document.querySelector("footer").classList.remove("_blur");
+    const $popupContainer = document.querySelector(containerSelector);
+    document.body.style.overflowY = 'auto';
+    $popupContainer.style.display = 'none';
+    document.querySelector(pageContentSelector).classList.remove('_blur');
+    document.querySelector('footer').classList.remove('_blur');
 }
 
 function showPopup(popupSelector) {
-  const $popup = document.querySelector(popupSelector);
-  $popup.style.display = "block";
+    const $popup = document.querySelector(popupSelector);
+    $popup.style.display = 'block';
 }
 
 function hidePopup(popupSelector) {
-  const $popup = document.querySelector(popupSelector);
-  $popup.style.display = "none";
+    const $popup = document.querySelector(popupSelector);
+    $popup.style.display = 'none';
 }
 
-function formatPhoneAsYouType(value, countryCode = "RU") {
-  return new AsYouType(countryCode).input(value);
+function formatPhoneAsYouType(value, countryCode = 'RU') {
+    return new AsYouType(countryCode).input(value);
 }
 
 function handleSubmit(form, sendCallback) {
-  const formData = new FormData();
-  Array.from(form.elements).forEach((item) => {
-    if (item.tagName === "INPUT") {
-      formData.append(item.name, item.value);
-    }
-  });
+    const formData = new FormData();
+    Array.from(form.elements).forEach((item) => {
+        if (item.tagName === 'INPUT') {
+            formData.append(item.name, item.value);
+        }
+    });
 
-  sendCallback(formData);
+    sendCallback(formData);
 }
 
 export {
-  toggleActive,
-  hidePopup,
-  showPopup,
-  formatPhoneAsYouType,
-  handleSubmit,
-  printFormData,
-  showPopupContainer,
-  hidePopupContainer,
+    hidePopup,
+    showPopup,
+    formatPhoneAsYouType,
+    handleSubmit,
+    printFormData,
+    showPopupContainer,
+    hidePopupContainer,
 };
